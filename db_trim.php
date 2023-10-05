03882cfe240a2148915f1cf53fe1796ad9fedf78
F. Siswanto <blackmonk88@users.noreply.github.com>
2023-10-05 12:21:38 +0700

    Update db_trim.php

--- db_trim.php
+++ db_trim.php
@@ -3,3 +3,23 @@
  * DB cleaner for very big groundhogg logs
  * Author : Fany Siswanto <blackmonk88@gmail.com>
  */
+
+// config defined here
+$trim_interval = '3 month';
+
+// Load WordPress
+define('WP_USE_THEMES', false);
+
+require_once('wp-load.php');
+global $wpdb;
+
+$query_trim_aws_email_queue = "SELECT * FROM `{$wpdb->prefix}_gh_aws_email_queue` WHERE `status` = 'failed' AND `time` <= NOW() - INTERVAL '{$trim_interval}';";
+
+// Execute the query
+$trim_aws_email_queue_rows = $wpdb->query($query_trim_aws_email_queue);
+
+if ($trim_aws_email_queue_rows === false) {
+    echo "An error occurred while deleting AWS Email Queues.<br />";
+} else {
+    echo "AWS Email Queue: $trim_aws_email_queue_rows rows deleted successfully.<br />";
+}
